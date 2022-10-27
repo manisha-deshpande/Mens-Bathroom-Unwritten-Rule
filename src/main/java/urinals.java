47571932a18c2fa82254ca4cc1608369eecfb8f5
mdeshp10 <112604545+mdeshp10@users.noreply.github.com>
2022-10-27 14:36:06 -0700

    added input validity and its test case

--- src/main/java/urinals.java
+++ src/main/java/urinals.java
@@ -1,16 +1,63 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class urinals {
+    static String invalidInput = "Invalid input";
     public static void main(String[] args){
         execute();
     }
-    public static void execute(){
-        List<String> allInputs = getAllInputsFromFile("src\\main\\resources\\urinal.dat");
+    public static void execute() {
+        try{
+            List<String> allInputs = getAllInputsFromFile("src\\main\\resources\\urinal.dat");
+            List<String> allOutputs = new ArrayList<>();
+
+            for(String input: allInputs){
+                if(invalidInput.equalsIgnoreCase(input))
+                    allOutputs.add(input);
+                else{
+                    allOutputs.add(findVacantUrinals(input));
+                }
+            }
+        }
+        catch(Exception e){
+
+        }
     }
 
-    public static List<String> getAllInputsFromFile(String filename) {
+    public static String findVacantUrinals(String input) {
+        int count = 0;
+        return String.valueOf(count);
+    }
+
+    public static List<String> getAllInputsFromFile(String filename) throws FileNotFoundException {
         List<String> response = new ArrayList<>();
-        return null;
+
+        Scanner scan;
+        File file = new File(filename);
+        scan = new Scanner(file);
+        String line;
+        while (scan.hasNextLine()) {
+            line = scan.nextLine();
+            response.add(checkInputValidity(line));
+        }
+        if (scan != null)
+            scan.close();
+
+        return response;
+    }
+
+    public static String checkInputValidity(String input) {
+        Pattern p = Pattern.compile("[01]+");
+        Matcher m = p.matcher(input);
+        boolean b = m.matches();
+
+        if(b==false){
+            return invalidInput;
+        }
+        return input;
     }
 }
--- src/test/java/urinalsTest.java
+++ src/test/java/urinalsTest.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class urinalsTest {
         @Test
-        public void testGetAllInputsFromFile(){
+        public void testGetAllInputsFromFile() throws FileNotFoundException {
             assertEquals(new ArrayList<>(),urinals.getAllInputsFromFile("src\\main\\resources\\urinal.dat"));
+        }
+
+        @Test(expected = FileNotFoundException.class)
+        public void testGetAllInputsFromFileNotFound() throws FileNotFoundException {
+            assertEquals(new ArrayList<>(),urinals.getAllInputsFromFile("src\\main\\resources\\testing.dat"));
+        }
+
+        @Test
+        public void testCheckInputValidity(){
+            assertEquals(urinals.invalidInput,urinals.checkInputValidity("AB1010"));
+            assertEquals("1001",urinals.checkInputValidity("1001"));
         }
 }
 
